18bb9454cb38ce89405b55594587d7ffd6414f71
Paulo <paulo.phc666@gmail.com>
2025-05-27 09:09:09 -0300

    docker config

--- src/main/java/com/app/rekognition/demo/controller/ImageController.java
+++ src/main/java/com/app/rekognition/demo/controller/ImageController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/image")
 public class ImageController {
